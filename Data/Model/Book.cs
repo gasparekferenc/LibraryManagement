cbde8c417f3b1c0ed1b5e45c6f4402660538e7ae
Gasparek Ferenc <gasparekferenc@Gasparek-iPro.forum.local>
2020-02-17 17:37:03 +0100

    x

--- Data/Model/Book.cs
+++ Data/Model/Book.cs
@@ -14,6 +14,7 @@ namespace LibraryManagement.Data.Model
         [MinLength(3)]
         [MaxLength(50)]
         public string Title { get; set; }
+
         public virtual Author Author { get; set; }
         public int AuthorId { get; set; }
         public Customer Borrower { get; set; }
